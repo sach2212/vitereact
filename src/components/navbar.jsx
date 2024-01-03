85ba18c5e3a163635e3256151efdcabd83fd16ff
Sachin <121556074+sach2212@users.noreply.github.com>
2024-01-03 20:45:12 +0530

    swecond commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+// import { RiMenu3Fill } from "react-icons/ri";
+ 
+// import './navbar.css'
+// import {useState} from "react";
+// import logo from "./../../assets/updt/upLOGO-HILLTONE-BLACK.png"
+// // import { IoClose } from "react-icons/io5";
+
+
+// import { Link } from "react-router-dom"
+//   function Navbar() {
+//   const [nav,setnav]= useState(false)
+//   const toogle =()=> setnav(!nav)
+  
+//     return (
+//     <div className="navbar">
+
+//       <ul className="nav-menu">
+     
+//         <li> <Link to= '/'> home</Link>  </li>
+//         <li> <Link to= '/about'> about</Link>  </li>
+   
+//         <div className="logo">
+        
+//         <img src={logo} alt="des"></img>
+                
+//         <div className="menu-btn"  onClick={toogle} >
+//         <RiMenu3Fill className="icon"/>
+        
+//       </div>
+//       </div>
+//       <li> <Link to= '/property'> property</Link>  </li>
+//         <li> <Link to= '/contact'> contact</Link>  </li>
+     
+//       </ul>
+
+      
+
+//       <div className="overlay"></div>
+      
+
+//       <div className={nav?'mobile_menu active' : 'mobile_menu'}>
+//       <IoClose onClick={ () => setnav(!nav)  }/>
+
+//         <ul className="mobile_ul">
+//         <li> <Link to= '/'> home</Link>  </li>
+//         <li> <Link to= '/about'> about</Link>  </li>
+      
+//        <li> <Link to= '/property'> property</Link>  </li>
+//         <li> <Link to= '/contact'> contact</Link>  </li>
+
+       
+//         </ul> 
+
+//       </div>
+
+
+
+// </div>
+       
+
+//   );
+// }
+
+// export default Navbar;
+
+ 
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
+ 
+
